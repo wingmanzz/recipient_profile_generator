5fd6b5d2c1cfd288c5d03d5a09292730fd3136df
kelvinabrokwa <kelvinabrokwa@gmail.com>
2015-12-04 14:04:23 -0500

    parse top3 data

--- scripts/parse_top3.js
+++ scripts/parse_top3.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 var path = require('path');
 var parser = require('csv-parse');
-var queue = require('queue-async');
+var async = require('async');
 
 try {
   fs.mkdirSync(path.join(__dirname, 'parsed_data'));
@@ -10,10 +10,11 @@ catch(e) {
   if (e.code !== 'EEXIST') throw e;
 }
 
-queue()
-  .defer(parseCSV, read('WBG_Region_top_3.csv'))
-  .defer(parseCSV, read('WBG_Region.csv'))
-  .awaitAll(join);
+async.map([
+    read('WBG_Region_top_3.csv'),
+    read('WBG_Region.csv') ],
+    parseCSV,
+    join);
 
 function parseCSV(csv, cb) {
   var data = [];
@@ -45,16 +46,49 @@ function parseCSV(csv, cb) {
     cb(err.message);
   });
 
+  csvParser.write(csv);
+  csvParser.end();
 }
 
 function join(err, data) {
   if (err) throw err;
   var top3 = data[0];
-  var region = data[1];
-  console.log(top3);
-  console.log(region);
+  top3 = top3.reduce(function(out, curr) {
+    if (!(curr.WBG_Region in out))
+    {
+      out[curr.WBG_Region] = {};
+      for (var i = 0; i < top3.length; i++)
+      {
+        var rank = top3[i];
+        if (rank.WBG_Region === curr.WBG_Region)
+        {
+          if (!(rank.q in out[curr.WBG_Region]))
+          {
+            out[curr.WBG_Region][rank.q] = [];
+          }
+          out[curr.WBG_Region][rank.q].push(rank);
+        }
+      }
+    }
+    return out;
+  }, {});
+  var countries = data[1];
+  countries = countries.map(function(c) {
+    c.rankings = top3[c.WBG_Region];
+    return c;
+  });
+
+  write(countries);
 }
 
 function read(fname) {
-  return fs.readFileSync(path.join(__dirname, '..', 'data', fname), { encoding: 'utf-8' });
+  return fs.readFileSync(path.join(
+        __dirname, '..', 'data', fname), { encoding: 'utf-8' });
+}
+
+function write(data) {
+  fs.writeFileSync(path.join(
+      __dirname, '..', 'data', 'rankings.json'),
+      JSON.stringify(data),
+      { encoding: 'utf-8' });
 }

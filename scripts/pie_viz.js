fc707249720862beaba013999bd4aca1de416d7e
kelvinabrokwa <kelvinabrokwa@gmail.com>
2016-01-20 11:49:28 -0500

    lint

--- scripts/pie_viz.js
+++ scripts/pie_viz.js
@@ -20,44 +20,39 @@ var orgtype = JSON.parse(fs.readFileSync(
 fetchRecipients()
   .then(generatePieData)
   .then(writeChartsToDisk)
-  .catch(function(err) { console.log("error"); });
+  .catch(function(err) { console.log(err); });
 
 
-function findDonor(cw, oid)
-{
+function findDonor(cw, oid) {
 	if (oid == -1)
-		return ("Other");
-	for (var i = 0; i < cw.hits.length; i++)
-	{
-		if (cw.hits[i]['id'] == oid)
-		{
-			return cw.hits[i]['name']
+		return ('Other');
+	for (var i = 0; i < cw.hits.length; i++) {
+		if (cw.hits[i]['id'] == oid) {
+			return cw.hits[i]['name'];
 		}
 	}
-	return -1
+	return -1;
 }
-function getODAById (rcvId, type)
-{
-	var alloda= [];
 
+function getODAById (rcvId, type) {
+	var alloda = [];
 
-    console.log("Getting Top ODA for: "+rcvId,type);
-	var url = 'http://api.aiddata.org/flows/origin?ro='+rcvId+'&y=2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013';
+  console.log('Getting Top ODA for: ' + rcvId, type);
+	var url = 'http://api.aiddata.org/flows/origin?ro=' + rcvId + '&y=2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013';
 	var res = request('GET', url);
-	var oda_json = JSON.parse(res.getBody('utf8'));
-	for (var i = 0; i < oda_json.item_count; i++)
-	{
-		var oda = {id: oda_json.items[i].source._id,
-			type:'None',
-			oda: oda_json.items[i].total};
+	var odaJSON = JSON.parse(res.getBody('utf8'));
+	for (var i = 0; i < odaJSON.item_count; i++) {
+		var oda = {
+      id: odaJSON.items[i].source._id, // eslint-disable-line no-underscore-dangle
+			type: 'None',
+			oda: odaJSON.items[i].total
+    };
 
 		// find type (dac/nondac etc)
-		for (var x=0; x<orgtype.length; x++)
-		{
-			if (orgtype[x].id == oda.id && orgtype[x].type == type)
-			{
+		for (var x = 0; x < orgtype.length; x++) {
+			if (orgtype[x].id == oda.id && orgtype[x].type == type) {
 				oda.type = orgtype[x].type;
-				alloda.push(oda)
+				alloda.push(oda);
 			}
 		}
 	}
@@ -70,33 +65,32 @@ function generatePieData(data) {
   return new Promise(function(resolve) {
     resolve(recipients.map(function(d) {
       function getData(type) {
-      	oda = getODAById(d.id, type);
+        var oda = getODAById(d.id, type);
 
-      	var other = {id:-1, oda:0};
-      	for (v = 5; v < oda.length; v++)
-      	{
-      		other.oda = other.oda + oda[v].oda;
-      	}
-
-      	function order(a, b) { return b.amount - a.amount; }
-  		oda = R.take(5, R.sort(order, oda));
-  		if (other.oda > 0)
-  			oda.push(other)
-        return oda.map(function(d) {
-          return {
-            donor: findDonor(donorcw, d.id),
-            amount: d.oda
-          };
-        });
-      }
-      return {
-        recipient: d.name,
-        donors: {
-          dac: getData('DAC'),
-          multi: getData('Multilateral'),
-          nondac: getData('Non-DAC')
+        var other = { id: -1, oda: 0 };
+        for (var v = 5; v < oda.length; v++) {
+          other.oda = other.oda + oda[v].oda;
         }
-      };
+
+        function order(a, b) { return b.amount - a.amount; }
+        oda = R.take(5, R.sort(order, oda));
+        if (other.oda > 0)
+          oda.push(other);
+          return oda.map(function(d) { // eslint-disable-line no-shadow
+            return {
+              donor: findDonor(donorcw, d.id),
+              amount: d.oda
+            };
+          });
+        }
+        return {
+          recipient: d.name,
+          donors: {
+            dac: getData('DAC'),
+            multi: getData('Multilateral'),
+            nondac: getData('Non-DAC')
+          }
+        };
     }));
   });
 }
@@ -144,20 +138,19 @@ function writeChart(i) {
     }
   });
 }
-function formatMoney( money )
-{
-	if (money > 1000000)
-	{
-		money = Math.round(money/1000000);
-		money = "$"+money+"M";
+
+function formatMoney( money ) {
+	if (money > 1000000) {
+		money = Math.round(money / 1000000);
+		money = '$' + money + 'M';
 	}
-	else
-	{
-		money = Math.round(money/1000);
-		money = "$"+money+"K";
+	else {
+		money = Math.round(money / 1000);
+		money = '$' + money + 'K';
 	}
 	return money;
 }
+
 function getChart(window, data) {
   var d3 = window.d3;
   var PX_RATIO = 4 / 3;
